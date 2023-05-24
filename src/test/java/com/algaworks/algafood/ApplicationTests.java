483fd303cf2a2abeb9da9db87f0ec0a11591e2f7
Leonardo Faria <leonardo.faria@e-unicred.com.br>
2023-05-24 14:36:52 -0300

    Defined different yml for ambient

--- src/test/java/com/algaworks/algafood/ApplicationTests.java
+++ src/test/java/com/algaworks/algafood/ApplicationTests.java
@@ -2,8 +2,10 @@ package com.algaworks.algafood;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("integration-test")
 class ApplicationTests {
 
 	@Test
